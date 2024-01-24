40fd84dc43f6bedef5936ddf79e1b33501c2f7c4
Tom Platt <thomasjplatt0107@gmail.com>
2024-01-24 14:00:53 +0000

    server created and connected to ongoDB with mongoose

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express, { Router } from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+const port = process.env.PORT || 6000;
+
+// Enables app to use JSON
+app.use(express.json());
+
+// app.use("/tables", router);
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected!"),
+      app.listen(port, () => console.log(`listening on port: ${port}`));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
